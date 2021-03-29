e641f84c7622df5ed302d3fc3afa9e1878bf871d
skyper-cz <62872636+skyper-cz@users.noreply.github.com>
2021-03-29 14:30:48 +0200

    Update SumSumsOfEvenAndMultipleOfOdd.java

--- src/main/java/cz/educanet/praha/SumSumsOfEvenAndMultipleOfOdd.java
+++ src/main/java/cz/educanet/praha/SumSumsOfEvenAndMultipleOfOdd.java
@@ -3,9 +3,35 @@ package cz.educanet.praha;
 import java.util.Scanner;
 
 public class SumSumsOfEvenAndMultipleOfOdd {
+    public static int sudy;
+    public static int lichy;
+
 
     public static int sumSumsOfEvenAndMultipleOfOdd(Scanner scanner) {
-        return 0;
+
+
+        while (scanner.hasNext()) {
+            int mezicast = scanner.nextInt();
+            if (mezicast % 2 == 0) {
+
+                System.out.println("The given number " + mezicast + " is Even ");
+                sudy += mezicast;
+
+            } else {
+
+                System.out.println("The given number " + mezicast + " is Odd ");
+                if (lichy == 0) {
+                    lichy = mezicast;
+                }
+                else {
+                    lichy *= mezicast;
+                }
+            }
+        }
+        int vysledek = sudy + lichy;
+        sudy = 0;
+        lichy = 0;
+        return vysledek;
     }
 
     public static void main(String[] args) {
